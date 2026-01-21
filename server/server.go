503020d26a35a59f6538e8e7fc8f3559fa41e709
Fernando Perry <fernandoperry1234@gmail.com>
2026-01-21 15:19:31 +0700

    add cors stuff

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 	"log"
@@ -31,6 +32,12 @@ func InitServer(url string, password string) *Server {
 	app := fiber.New(fiber.Config{
 		AppName: "GopherDrop Backend Ow0",
 	})
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+        AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+        AllowCredentials: false,
+    }))
 	return &Server{
 		App:        app,
 		Url:        url,
